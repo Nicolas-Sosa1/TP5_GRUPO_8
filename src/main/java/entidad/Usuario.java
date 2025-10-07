d685b3eb794b66a7da86661fdf4a47e10a104cdd
pedropursall <pedropursall@gmail.com>
2025-10-07 01:13:37 -0300

    Clase Usuario creada

---
+++ src/main/java/entidad/Usuario.java
@@ -0,0 +1,44 @@
+package entidad;
+
+public class Usuario {
+    private int idUsuario;
+    private String nombre;
+    private String contrasena;
+
+    public Usuario() {}
+
+    public Usuario(int idUsuario, String nombre, String contrasena) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsuario() {
+        return nombre;
+    }
+
+    public void setUsuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [idUsuario=" + idUsuario + ", nombre=" + nombre + ", contrasena=" + contrasena + "]";
+    }
+}
